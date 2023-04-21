67d1b751e30eb7cf8819f3a4ab3992a5cd6ea427
BraedenHCL <braeden.norman6@gmail.com>
2023-04-20 20:50:19 -0700

    reorganize package

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="TPL-package",
+    name="TPL_package",
     version="0.0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
+    description='An package used for the TPL project',
+    license='MIT',
     install_requires=[
         "fastapi",
         "firebase-admin",
         "google",
         "pydantic",
-    ]
+    ],
+    url="https://github.com/The-Programming-Lab/TPL_package",
+    author="The Programming Lab",
+    author_email="braeden.norman6@gmail.com"
 )
